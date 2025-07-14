f05ae6e96cd160ed3f5f99e6e37a4cb5276c7b84
jgmelo <jgmelo@gmail.com>
2025-07-14 00:15:57 -0300

    Fixed Angular Houses Metric - id attribute of the House object.

--- chart/metrics/angular.py
+++ chart/metrics/angular.py
@@ -8,6 +8,6 @@ class AngularHouses(Metric):
     name = "angular_houses"
 
     def compute(self, chart_data):
-        angular_nums = {1, 4, 7, 10}
+        angular_houses = {"House1", "House4", "House7", "House10"}
         planets = chart_data.get("planets", [])
-        return [p["name"] for p in planets if p.get("house") in angular_nums]
+        return [p["name"] for p in planets if p.get("house").id in angular_houses]
