23855242f509a165877b8e9a139b9ca13e1c5b94
jgmelo <46022820+jgmelo@users.noreply.github.com>
2025-07-14 00:40:57 -0300

    Add aspects metric and refine AngularHouses

--- chart/metrics/__init__.py
+++ chart/metrics/__init__.py
@@ -1,6 +1,7 @@
 """Chart metrics package."""
 
-from . import angular  # noqa: F401  - register metric classes
+from . import angular  # noqa: F401 - register metric classes
+from . import aspects  # noqa: F401 - register metric classes
 from .base import compute_all
 
 __all__ = ["compute_all"]
